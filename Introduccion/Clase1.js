6fb3e8f45d69935995c5524cabdfb088190934e0
Your Name <javierconpec@gmail.com>
2025-05-17 23:30:44 -0500

    primera clase

---
+++ Introduccion/Clase1.js
@@ -0,0 +1,63 @@
+//OPERADORES :
+//SUMA
+1+1
+//RESTA
+1-1
+//MULTIPLICACION
+1*1
+//DIVISION
+1/1
+//RESTO
+1%1
+//EXPONENTE
+1**1
+
+//STRING:   son cadenas de texto
+"Javier Contreras"
+//CONCATENAR:   juntar cadenas de textos con el simbolo "+"
+"javier" + "contreras"
+//BOOLEANOS:     Es para validar si es verdadero o falso
+//Unicos valores:
+true + "y" + false
+
+// OPERADORES DE COMPARACION:
+//igual
+"javier" === "javi"
+//diferente
+"javier" !== "javi"
+//mayor que 
+2>=1
+//menor que
+2<=2
+
+//OPERADORES LOGICOS
+//and
+(1+2) && (2+3)
+//or
+(1+1) || (2+2)
+//not:   se coloca antes del booleano
+!true
+
+
+//VARIABLES:    son contenedores donde podremos guardar valores para reutilizarlos
+//tipos:
+//Let
+let nombre = "javier"
+//Const: es una constante es decir siempre tendra ese valor y no se pdora cambias
+const numero1 = 2
+
+//CONDICIONALES:    son codigo que se ejecutaran dado si se cumple la condicion.
+//Tipos:
+if(1+1){
+    "true"
+}
+while(1+1)[
+    "true"
+]
+
+//BUCLES:    son codigos que se ejecutaran hasta que se cumpla la condicion.
+//tipos
+//FOR:
+for(let day = 0; day < 4; day=day+1){
+    console.log("hola")
+}
